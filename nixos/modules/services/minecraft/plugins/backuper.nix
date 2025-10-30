f9c2e743e9dc09310f2fbc78641ab05e269ff5ec
Version33 <vee@versionthirtythr.ee>
2025-10-30 00:39:24 -0400

    update plugin paths

--- nixos/modules/services/minecraft/plugins/backuper.nix
+++ nixos/modules/services/minecraft/plugins/backuper.nix
@@ -2,8 +2,8 @@
 
 let
   backuper = pkgs.fetchurl {
-    url = "https://github.com/DVDishka/Backuper";
-    sha256 = "";
+    url = "https://github.com/DVDishka/Backuper/releases/download/3.4.5/Backuper-3.4.5.jar";
+    sha256 = "Zi5Tv5vbB1a7vrg3NsHax6UJb47dmvCJTPPnVSV366Y=";
   };
 in {
   systemd.tmpfiles.rules = [
