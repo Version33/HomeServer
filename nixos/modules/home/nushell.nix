3d0bda3f3eab8d56426b9b4a4aff2e467b26ea69
Version33 <vee@versionthirtythr.ee>
2025-10-29 21:35:20 -0400

    mc-cmd update

--- nixos/modules/home/nushell.nix
+++ nixos/modules/home/nushell.nix
@@ -5,10 +5,13 @@
     enable = true;
     extraConfig = ''
       $env.config.show_banner = false
+      
+      def mc-cmd [...args: string] {
+        mcrcon -H localhost -P 25575 -p changeme ($args | str join ' ')
+      }
     '';
     shellAliases = {
       mc-console = "mcrcon -H localhost -P 25575 -p changeme";
-      mc-cmd = "mcrcon -H localhost -P 25575 -p changeme";
       mc-logs = "journalctl -u minecraft-server -f";
     };
   };
