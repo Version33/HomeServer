4055f35f25716433291db6438b0a3cca3f7dfa72
Version33 <vee@versionthirtythr.ee>
2025-10-30 00:28:37 -0400

    change source

--- nixos/modules/services/minecraft/plugins/tabtps.nix
+++ nixos/modules/services/minecraft/plugins/tabtps.nix
@@ -2,8 +2,7 @@
 
 let
   tabtps = pkgs.fetchurl {
-    url =
-      "https://hangarcdn.papermc.io/plugins/jmp/TabTPS/versions/1.3.29/PAPER/tabtps-paper-1.3.29.jar";
+    url = "https://github.com/jpenilla/TabTPS";
     sha256 = "5ap38Up/M6SDTSUZK5LHpFKh7ic3hkCVsCF7AYGp3NA=";
   };
 in {
