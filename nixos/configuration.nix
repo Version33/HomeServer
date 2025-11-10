0953a9c0f6e09f24811f3cb40f53c3cbac78a358
Version33 <vee@versionthirtythr.ee>
2025-11-10 01:36:11 -0500

    add sk

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -103,8 +103,8 @@
     extraGroups = [ "wheel" "media" ];
     shell = pkgs.nushell;
     openssh.authorizedKeys.keys = [
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINfIKVYa5elcDEfmyfn+ODALz1Sy+OcajNskRxahTFOpAAAABHNzaDo= vee@homeserver"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINvec5l0CVyepm8MyoLJ0xrl5nJqztj7eul7HYsVV9zc vee@k0or"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIBRq+GQ8d8WXy1r12g6MxXHv21HqsU0K1QYRecRWPe1zAAAADnNzaDpob21lc2VydmVy vee@solokey2"
     ];
   };
 
