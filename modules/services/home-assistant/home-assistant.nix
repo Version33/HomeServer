2742c73ecc32e0c23345cf7c5acf2c24a8db0072
Version33 <vee@versionthirtythr.ee>
2026-03-22 03:16:08 -0400

    +thinq

--- modules/services/home-assistant/home-assistant.nix
+++ modules/services/home-assistant/home-assistant.nix
@@ -1,42 +1,45 @@
 {
 
-  flake.modules.nixos.home-assistant = { config, lib, ... }: {
-    services.home-assistant = {
-      enable = true;
-      openFirewall = true;
-      extraComponents = [
-        "met"
-        "esphome"
-        "zwave_js"
-	"isal"
-	"radio_browser"
-	"zha"
-	"sun"
-	"mobile_app"
-	"bluetooth"
-      ];
-      config = {
-        default_config = { };
-        http = {
-          server_host = "0.0.0.0";
-          server_port = 8123;
-          trusted_proxies = [ "127.0.0.1" ];
-          use_x_forwarded_for = true;
+  flake.modules.nixos.home-assistant =
+    { config, lib, ... }:
+    {
+      services.home-assistant = {
+        enable = true;
+        openFirewall = true;
+        extraComponents = [
+          "met"
+          "esphome"
+          "zwave_js"
+          "isal"
+          "radio_browser"
+          "zha"
+          "sun"
+          "mobile_app"
+          "bluetooth"
+          "lg_thinq"
+        ];
+        config = {
+          default_config = { };
+          http = {
+            server_host = "0.0.0.0";
+            server_port = 8123;
+            trusted_proxies = [ "127.0.0.1" ];
+            use_x_forwarded_for = true;
+          };
+        };
+      };
+
+      users.users.hass = lib.mkIf config.services.home-assistant.enable {
+        extraGroups = [ "dialout" ];
+      };
+
+      services.caddy.virtualHosts = {
+        "hass.versionthirtythr.ee" = {
+          extraConfig = ''
+            reverse_proxy http://localhost:8123
+          '';
         };
       };
     };
-
-    users.users.hass = lib.mkIf config.services.home-assistant.enable {
-      extraGroups = [ "dialout" ];
-    };
-
-    services.caddy.virtualHosts = {
-      "hass.versionthirtythr.ee" = {
-        extraConfig = ''
-          reverse_proxy http://localhost:8123
-        '';
-      };
-    };
-  };
 
 }
