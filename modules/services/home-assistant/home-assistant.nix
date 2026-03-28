dc20d59b0fac28ec4847ecd61b6035663672c3c8
Version33 <vee@versionthirtythr.ee>
2026-03-27 20:40:04 -0400

    testing

--- modules/services/home-assistant/home-assistant.nix
+++ modules/services/home-assistant/home-assistant.nix
@@ -26,6 +26,12 @@
             trusted_proxies = [ "127.0.0.1" ];
             use_x_forwarded_for = true;
           };
+          advanced = {
+            channel = 11;
+            pan_id = 6754;
+            ext_pan_id = "00124b0030dd86a9";
+            network_key = [ 1 3 5 7 9 11 13 15 0 2 4 6 8 10 12 13 ];
+          };
         };
       };
 
